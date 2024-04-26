82615a0ff8bb32db9aa6cf0c4b2bdd63067164e7
Alex <Oleksii.karamanov@powercoders.org>
2024-04-26 12:45:40 +0200

    template structure

--- script.js
+++ script.js
@@ -6,14 +6,24 @@ let images = [
 
 let names = ["Alona", "Ana", "Burak"];
 
-for (i = 0; i < images.length; i++) {
-  let section = document.querySelector("section");
-  let image = document.createElement("img");
-  let text = document.createElement("h3");
-  image.className = "image";
-  image.setAttribute("src", images[i]);
-  text.textContent = names[i];
-  section.appendChild(image);
-  section.appendChild(text);
-  image.style.cssText = "width: 300px;";
+
+let persons = [
+  {
+    name: "Alona",
+    img: "./images/Alona-1.jpg"
+  }
+]
+function buildPage() {
+  for (i = 0; i < images.length; i++) {
+    let section = document.querySelector("section");
+
+    let image = document.createElement("img");
+    let text = document.createElement("h3");
+    image.className = "image";
+    image.setAttribute("src", images[i]);
+    text.textContent = names[i];
+    section.appendChild(image);
+    section.appendChild(text);
+    image.style.cssText = "width: 300px;";
+  }
 }
