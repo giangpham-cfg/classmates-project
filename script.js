014892060ffe2e2af0f000f1057d1b55db5d406c
GiangPham <giang.pham@powercoders.org>
2024-04-26 12:22:39 +0200

    Update

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ let images = [
   "./images/Ana-1.jpg",
   "./images/Burak-1.jpg",
 ];
-let section = document.querySelector("section");
-// console.log("section");
+
+let names = ["Alona", "Ana", "Burak"];
 
 for (i = 0; i < images.length; i++) {
+  let section = document.querySelector("section");
   let image = document.createElement("img");
+  let text = document.createElement("h3");
   image.className = "image";
   image.setAttribute("src", images[i]);
+  text.textContent = names[i];
   section.appendChild(image);
+  section.appendChild(text);
+  image.style.cssText = "width: 300px;";
 }
